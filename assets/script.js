093a1dfba65a8801c8c3fcdd57d1e46b2afb9a27
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-01 17:18:41 -0600

    added function to make startScreen hidden on button click and show quiz screen

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
+var startScreen = document.getElementById("startScreenId");
+var quizScreen = document.getElementById("quizId");
+var scoresScreen = document.getElementById("scoresId");
 var startButton = document.getElementById("start");
+
+quizScreen.style.visibility = "hidden";
+quizScreen.style.visibility = "hidden";
 
 startButton.addEventListener("click", function () {
   console.log("clicked!!");
+  startScreen.style.visibility = "hidden";
+  quizScreen.style.visibility = "visible";
 });
